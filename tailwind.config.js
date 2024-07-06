74a6d206266d4af16eb83048a9aa28178f6df849
Handi-Putra <43770290+Handi-Putra@users.noreply.github.com>
2024-07-06 18:45:42 +0700

    Finish

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "mainBackgroundColor": '#0D1117',
+        "columnBackgroundColor": '#161C22'
+      },
+      backgroundImage: {
+        'custom-bg': "url('https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')"
+      }
+    },
+  },
+  plugins: [],
+}
+
